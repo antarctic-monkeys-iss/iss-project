35fcd37b1049b619d1024c3f9f21d11bb2908b95
harshithseera <164095439+antarctic-monkeys-iss@users.noreply.github.com>
2024-03-20 16:16:20 +0530

    Disabled ssl verification

--- app3.py
+++ app3.py
@@ -15,7 +15,7 @@ import base64
 app = Flask(__name__)
 
 # Configure SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = "cockroachdb://antarctic:HBVCEcPaB0kquTdTlW8lTw@issproject-9037.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/project?sslmode=verify-full"
+app.config['SQLALCHEMY_DATABASE_URI'] = "cockroachdb://antarctic:HBVCEcPaB0kquTdTlW8lTw@issproject-9037.8nk.gcp-asia-southeast1.cockroachlabs.cloud:26257/project?sslmode=disable"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 
